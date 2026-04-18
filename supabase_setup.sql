c5b5c5f08e22f92d658ca9479cc632bc5feb2d70
Anup N <anupnedu@gmail.com>
2026-04-18 19:04:19 +0530

    re-enter

--- supabase_setup.sql
+++ supabase_setup.sql
@@ -290,3 +290,4 @@ grant usage on schema public to postgres, anon, authenticated, service_role;
 grant all privileges on all tables in schema public to postgres, anon, authenticated, service_role;
 grant all privileges on all functions in schema public to postgres, anon, authenticated, service_role;
 grant all privileges on all sequences in schema public to postgres, anon, authenticated, service_role;
+/*  */
